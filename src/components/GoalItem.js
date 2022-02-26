cd25d9c761c8a50512bab6f8ecd525a3ad5cb6d3
Nicola Hearn <72850665+NIcolaHearn@users.noreply.github.com>
2022-02-26 09:28:28 +0000

    Reset project to be completed alongside tutorial

---
+++ src/components/GoalItem.js
@@ -0,0 +1,15 @@
+import './GoalItem.css';
+
+function GoalItem() {
+    return (
+        <div className="goal-item">
+            <div>Title</div>
+            <div>
+                <h2>Target Date</h2>
+                <h2>Optional Comments</h2>
+            </div>
+        </div>
+    );
+};
+
+export default GoalItem;
